85981033b6b750d31e622c8e95b6b678286b61bb
AlexDoss42 <alex.doss42@gmail.com>
2024-07-01 16:34:57 -0600

    sets up some basic structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ function App() {
   return (
     <div className="App">
       <h1>LET HIM COOKBOOK!</h1>
+      <p>NOT the annoying life store section</p>
+      <h2>Recipe Name</h2>
+      <p>Recipe Author</p>
+      <h3>Ingredients List</h3>
+      <ul>
+        <li>stuff</li>
+        <li>more stuff</li>
+        <li>spicy stuff</li>
+        <li>sweet stuff</li>
+      </ul>
+      <h3>Cooking Instructions</h3>
+      <ol>
+        <li>get the stuff</li>
+        <li>prep the stuff</li>
+        <li>cook the stuff</li>
+        <li>serve the stuff</li>
+      </ol>
     </div>
   );
 }
