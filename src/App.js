57e014b186475f8abe8e9dc97d07b4546b3b7bd8
AlexDoss42 <alex.doss42@gmail.com>
2024-07-03 08:29:26 -0600

    gotta put the components on the page ya silly goose

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 
 import './App.css';
+import { Ingredients } from './Components/Ingredients';
+import { Instructions } from './Components/Instructions';
+import { LeaveAReview } from './Components/LeaveAReview';
+import { NutritionalFacts } from './Components/NutritionalFacts';
+import { ReviewHighlights } from './Components/ReviewHighlights';
+import { ReviewList } from './Components/ReviewList';
+import { NavBar } from './NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>LET HIM COOKBOOK!</h1>
       <p>NOT the annoying life store section</p>
       <h2>Recipe Name</h2>
       <p>Recipe Author</p>
+      <ReviewHighlights />
+      <NutritionalFacts />
+      <Ingredients />
+      <Instructions />
+      <LeaveAReview />
+      <ReviewList />
     </div>
+    
   );
 }
 
