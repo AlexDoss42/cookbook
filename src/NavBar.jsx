0917daa6afe956d247465c88c76d79a561dec833
AlexDoss42 <alex.doss42@gmail.com>
2024-07-03 08:30:10 -0600

    links are broken

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 export const NavBar = () => {
   return (
     <div> 
         <nav>           
             <ul>
-                <li><Link className='link' to="/">Home</Link></li>
+                {/* <li><Link className='link' to="/">Home</Link></li>
                 <li><Link className='link' to="/recipes">Recipes</Link></li>
                 <li><Link className='link' to="/tree">Equipment</Link></li>
-                <li><Link className='link' to="/search">About</Link></li>
+                <li><Link className='link' to="/search">About</Link></li> */}
             </ul>
         </nav>
     </div>
