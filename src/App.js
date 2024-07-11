6af982a7ff8d6db3dd33d65ef7fde67d2afb38da
AlexDoss42 <alex.doss42@gmail.com>
2024-07-11 16:42:21 -0600

    styles the recipe image a bit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <p>NOT the annoying life store section</p>
       <h2>{data.recipe_name}</h2>
       <p>{data.author}</p>
-      <img src={data.img_url} alt={data.recipe_name}/>
+      <img className='recipe-image' src={data.img_url} alt={data.recipe_name}/>
       <ReviewHighlights />
       <Time timeData={data.time}/>
       <NutritionalFacts nutritionalFacts={ data.nutritional_facts } servings={data.servings} />
