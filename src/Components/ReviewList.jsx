d88aef43934b63d39bf43ebf577afc4abbca5711
AlexDoss42 <alex.doss42@gmail.com>
2024-07-13 21:12:36 -0600

    removes a slash that was randomly in the review list

--- src/Components/ReviewList.jsx
+++ src/Components/ReviewList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ReviewList = () => {
   return (
-    <div className='section'>\
+    <div className='section'>
       <h3>Read the reviews</h3>
       <ul>
         <li></li>
