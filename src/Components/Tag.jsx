ecdafedac70b5041026b7a775748e2e6c0590bd8
AlexDoss42 <alex.doss42@gmail.com>
2024-07-13 21:32:04 -0600

    renders tag section

--- src/Components/Tag.jsx
+++ src/Components/Tag.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Tag = () => {
+export const Tag = ({data}) => {
   return (
-    <div>Tag</div>
+    <li>{data}</li>
   )
 }
--- src/Components/TagsList.jsx
+++ src/Components/TagsList.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Tag } from './Tag.jsx'
 
-export const TagsList = () => {
+export const TagsList = ({tagData}) => { 
+    const { tags } = tagData
   return (
     <div className='section'>
         <h2>Tags</h2>
-        
+        <ul>
+            {tags.map((tag) => {
+                return <Tag data={tag} />
+            })}
+        </ul>
     </div>
   )
 }
