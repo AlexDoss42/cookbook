d904ca8e445cb4e3ad6bc70a0a0174a37c507a75
AlexDoss42 <alex.doss42@gmail.com>
2024-07-13 21:18:24 -0600

    forgot a console.log

--- src/Components/Ingredients.jsx
+++ src/Components/Ingredients.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const Ingredients = ({ Ingredients }) => {
-  console.log(Ingredients)
   return (
     <div className='section'> 
         <h3>Ingredients List</h3>
