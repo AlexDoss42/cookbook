857c93cd0f6d6d69d206419ca88d732c1ecd7887
AlexDoss42 <alex.doss42@gmail.com>
2024-07-15 19:55:33 -0600

    gets the ingredients list left justified

--- src/Components/Ingredients.jsx
+++ src/Components/Ingredients.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Ingredients.css'
 
 export const Ingredients = ({ Ingredients }) => {
   return (
     <div className='section'> 
         <h3>Ingredients List</h3>
-        <ul>
+        <ul className='ingredients-list'>
           {Ingredients.map((ingredient) => {
             return<li>{ingredient}</li>
           })}
