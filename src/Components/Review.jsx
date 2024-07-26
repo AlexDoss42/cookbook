dd96e67682107ca40c5139d8bf8b3de3f85893fa
AlexDoss42 <alex.doss42@gmail.com>
2024-07-26 13:15:11 -0600

    cleans up some padding and other spacing/alignment

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -6,8 +6,8 @@ export const Review = ( { review_data }) => {
     const { title, author, rating, review } = review_data.review_data
   return (
     <div className='review-container'>
-        <h3>{title}</h3>
-        <p>{author}</p>
+        <h3 className='review-title'>{title}</h3>
+        <p className='review-author'>{author}</p>
         <div className='rating-date-container'>
         <StarRating rating={rating} />
         <span className='date'>date</span>
