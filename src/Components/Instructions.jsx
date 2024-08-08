a4e07a153e77ada5e631ba73eb61a14f4f07bdfd
AlexDoss42 <alex.doss42@gmail.com>
2024-08-08 16:57:30 -0600

    align text left on instructions

--- src/Components/Instructions.jsx
+++ src/Components/Instructions.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Instructions.css'
 
 export const Instructions = ({ Instructions }) => {
   return (
     <div className='section'>
       <h3>Cooking Instructions</h3>
-      <ol>
+      <ol className='instructions-list'>
         {Instructions.map((instruction) => {
               return<li>{instruction}</li>
             })}
