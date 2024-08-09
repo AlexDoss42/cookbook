7a12db29b35e16787b62a07aae696981770c53a4
AlexDoss42 <alex.doss42@gmail.com>
2024-08-09 09:24:43 -0600

    adds a title input on the review

--- src/Components/LeaveAReview.jsx
+++ src/Components/LeaveAReview.jsx
@@ -4,7 +4,7 @@ export const LeaveAReview = () => {
   return (
     <div className='section'>
     <h3>Leave a review</h3>
-    
+    <input type='text' name='title' />
     <textarea />
     </div>
   )
