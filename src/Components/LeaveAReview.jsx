1fdfa584e9353e8b7c1bfbeb4e2c98c54c81c3cf
AlexDoss42 <alex.doss42@gmail.com>
2024-08-13 17:12:55 -0600

    adds a title label to the input

--- src/Components/LeaveAReview.jsx
+++ src/Components/LeaveAReview.jsx
@@ -4,6 +4,7 @@ export const LeaveAReview = () => {
   return (
     <div className='section'>
     <h3>Leave a review</h3>
+    <label>Title: </label>
     <input type='text' name='title' />
     {/* place holder for 5 star rating component */}
     <p>*****</p> 
